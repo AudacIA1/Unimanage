086852566bdfb3bbc6e52de06a016c5184e69d7f
Eric De la Hoz <eric201708@gmail.com>
2025-11-10 11:38:26 -0500

    Cambios a settings

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-=6)e7g1exyjnb_8qqm#etcnp2#k=jwcf9koup8pay2vyxw_hoa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
